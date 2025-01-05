81003591926ec1c18bab60952f1f6e884ec7072d
naitsircnat <cristiangerardchen@gmail.com>
2025-01-05 09:22:09 +0000

    Completed exercise 3

--- 03-vending-machine/src/App.js
+++ 03-vending-machine/src/App.js
@@ -1,18 +1,22 @@
-import React from 'react';
+import React from "react";
+import { useState } from "react";
 
-export default function App () {
-  
+export default function App() {
+  const [displayMessage, setDisplayMessage] = useState("");
+
   return (
-      <React.Fragment>
-        <h1>Drink Vending Machine</h1>
-        <div className="display">
-        </div>
-        <button>Coffee</button>
-        <button>Tea</button>
-        <button>Orange Juice</button>
-      </React.Fragment>
-    );
-  
+    <React.Fragment>
+      <h1>Drink Vending Machine</h1>
+
+      <div className="display">{displayMessage}</div>
+
+      <button onClick={() => setDisplayMessage("Dispensing coffee")}>
+        Coffee
+      </button>
+      <button onClick={() => setDisplayMessage("Dispensing Tea")}>Tea</button>
+      <button onClick={() => setDisplayMessage("Dispensing Orange Juice")}>
+        Orange Juice
+      </button>
+    </React.Fragment>
+  );
 }
-
-
